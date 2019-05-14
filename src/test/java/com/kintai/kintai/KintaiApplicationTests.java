470e921cf2ba3cf57e4e94754175d9ffdb220920
yukiko bass <springcomesagain129@gmail.com>
2019-05-14 14:25:45 +0900

    inti

---
+++ src/test/java/com/kintai/kintai/KintaiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kintai.kintai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KintaiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
